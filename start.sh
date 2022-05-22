787617a83da25797d7dc7b52177101d03e50cc7a
malik7983 <103699299+malik7983@users.noreply.github.com>
2022-05-22 23:28:59 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/EvamariaTG/EvaMaria.git /EvaMaria
+  git clone https://github.com/malik7983/DQ-the-file-donor.git /DQ-the-file-donor
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
+  git clone $UPSTREAM_REPO /DQ-the-file-donor
 fi
-cd /EvaMaria
+cd /DQ-the-file-donor
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
